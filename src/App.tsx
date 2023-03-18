f9c0323c89a7b9df81ddd71f84d66f550d271d93
Tsumugi Saito <tsumugi.it@gmail.com>
2023-03-18 13:26:22 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
 
-function App() {
+const Container = styled.div`
+  background-color: #a1caf1;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: sans-serif;
+`;
+
+const Menu = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 2rem;
+`;
+
+const MenuItem = styled.button`
+  border: none;
+  background-color: inherit;
+  color: white;
+  font-size: 1.5rem;
+  padding: 0.5rem 1rem;
+  cursor: pointer;
+  transition: all 0.3s ease;
+
+  &:hover {
+    background-color: white;
+    color: #a1caf1;
+  }
+`;
+
+const TabContent = styled.div`
+  width: 80%;
+  background-color: white;
+  padding: 2rem;
+  border-radius: 0.5rem;
+  box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.3);
+  line-height: 1.5;
+`;
+
+const SkillsContent = () => (
+  <TabContent>
+    <h2>Skills</h2>
+    <ul>
+      <li>React</li>
+      <li>TypeScript</li>
+      <li>JavaScript</li>
+      <li>HTML</li>
+      <li>CSS</li>
+    </ul>
+  </TabContent>
+);
+
+const ExperienceContent = () => (
+  <TabContent>
+    <h2>Experience</h2>
+    <ul>
+      <li>Front-end engineer at Company A (2018-2020)</li>
+      <li>Front-end engineer at Company B (2020-2022)</li>
+      <li>Freelance front-end engineer (2022-present)</li>
+    </ul>
+  </TabContent>
+);
+
+const App = () => {
+  const [activeTab, setActiveTab] = useState("skills");
+
+  const handleTabClick = (tab: string) => {
+    setActiveTab(tab);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1>My Portfolio</h1>
+      <Menu>
+        <MenuItem onClick={() => handleTabClick("skills")}>Skills</MenuItem>
+        <MenuItem onClick={() => handleTabClick("experience")}>
+          Experience
+        </MenuItem>
+      </Menu>
+      {activeTab === "skills" ? <SkillsContent /> : <ExperienceContent />}
+    </Container>
   );
-}
+};
 
 export default App;
